28cd2eac38536bc66528b2530ef21e4c0b9bf686
Abdullah Shams Bin Tariq <asbtariq@ru.ac.bd>
2012-03-01 11:34:44 +0100

    plotComb.py looped over files in a list

--- run/plotComb.py
+++ run/plotComb.py
@@ -3,26 +3,20 @@
 import numpy
 from matplotlib import pyplot
 
-e1Dat = numpy.loadtxt( '../expDat/28Si26000.dat')
-t1Dat = numpy.loadtxt('../out/28Si26000run1.out')
-e2Dat = numpy.loadtxt( '../expDat/28Si26500.dat')
-t2Dat = numpy.loadtxt('../out/28Si26500run1.out')
-e3Dat = numpy.loadtxt( '../expDat/28Si27000.dat')
-t3Dat = numpy.loadtxt('../out/28Si27000run1.out')
+def plotMult(exF,thF):
+    fig = pyplot.figure()
+    for setId in range(len(exF)):
+        print 'Plotting Set ',setId,' ...'
+        eDat = numpy.loadtxt(exF[setId])
+        tDat = numpy.loadtxt(thF[setId])
+        subPlotIdx = str(len(exF))+'1'+str(setId+1)
+        subFig = fig.add_subplot(subPlotIdx)
+        ePlot = pyplot.semilogy(eDat[:,0],eDat[:,1],'go')
+        tPlot = pyplot.semilogy(tDat[:,0],tDat[:,1])
+    pyplot.show()
 
-fig1 = pyplot.figure(1)
-
-fig1a = fig1.add_subplot(311)
-fig1a = pyplot.semilogy(e1Dat[:,0],e1Dat[:,1],'go')
-fig1a = pyplot.semilogy(t1Dat[:,0],t1Dat[:,1])
-fig2a = fig1.add_subplot(312)
-fig2a = pyplot.semilogy(e2Dat[:,0],e2Dat[:,1],'go')
-fig2a = pyplot.semilogy(t2Dat[:,0],t2Dat[:,1])
-fig3a = fig1.add_subplot(313)
-fig3a = pyplot.semilogy(e3Dat[:,0],e3Dat[:,1],'go')
-fig3a = pyplot.semilogy(t3Dat[:,0],t3Dat[:,1])
-
-
-pyplot.show()
+exFiles = [ '../expDat/28Si26000.dat', '../expDat/28Si26500.dat', '../expDat/28Si27000.dat']
+thFiles = ['../out/28Si26000run1.out','../out/28Si26500run1.out','../out/28Si27000run1.out']
+plotMult(exFiles,thFiles)
 
 
